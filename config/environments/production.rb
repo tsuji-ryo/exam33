7eb548774e121b3ea07a035968dcd1de0d673a24
tsuji <>
2017-09-18 19:49:10 +0900

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'frozen-hamlet-68485.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'polar-escarpment-61266.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
